a80ad434b5eded4db465158cb8bb8ca2fbc77ceb
RenanCdS <renan.cds0911@gmail.com>
2021-01-31 01:15:18 -0300

    feature: validation on login

--- src/app/login/components/login-form/login-form.component.ts
+++ src/app/login/components/login-form/login-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'login-form',
@@ -9,13 +10,24 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginFormComponent implements OnInit {
 
   loginForm: FormGroup;
-  constructor(private readonly fb: FormBuilder) { }
+  constructor(private readonly fb: FormBuilder,
+    private readonly router: Router) { }
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      email: ['', Validators.email],
-      password: ['', Validators.minLength(7)],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(7)]],
     });
   }
 
+  login(): void {
+    if (!this.loginForm.valid) {
+      this.loginForm.markAllAsTouched();
+      this.loginForm.updateValueAndValidity();
+      return;
+    }
+    this.router.navigate(['/home']);
+    this.loginForm.reset();
+    this.loginForm.updateValueAndValidity();
+  }
 }
